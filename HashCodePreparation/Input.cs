b43b75ecd6b854d339c1d622fc4c58cad1ce7c71
Michael Shpilt <michaels@codevalue.net>
2018-02-08 19:33:51 +0200

    Input

---
+++ HashCodePreparation/Input.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashCodePreparation
+{
+    public class Endpoint
+    {
+        public int LantencyToDataCenter { get; set; }
+        Dictionary<int, int> LantencyToCacheCenter;//index is cache center, value is lantency
+    }
+
+    public class Request
+    {
+        public int NumRequests { get; set; }
+        public int NumVideo { get; set; }
+        public int NumEndpoint { get; set; }
+    }
+
+    public class Input
+    {
+        public int[] Movies { get; set; }//index is movie number, size is value
+        public int NumOfCacheCenters { get; set; }
+        public int CacheCenterSize { get; set; }
+        public Endpoint[] Endpoints { get; set; }
+        public Request[] Requests { get; set; }
+
+    }
+}
