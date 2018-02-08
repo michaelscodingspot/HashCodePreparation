82ebdaf8433fc620e0c6289d7688f6c5baa3dd98
Michael Shpilt <michaels@codevalue.net>
2018-02-08 19:36:24 +0200

    Result and IAlg

---
+++ HashCodePreparation/Result.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashCodePreparation
+{
+    public class Result
+    {
+        //index is cache center. Value is list of video indexes
+        public Dictionary<int, List<int>> Data { get; set; }
+    }
+}
