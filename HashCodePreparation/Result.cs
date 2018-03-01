893d51874c8c510451382d0a2fbe6fc2bcdcc3fc
Moaid Hathot <moaid.hathot@outlook.com>
2018-03-01 18:15:07 +0200

    try push

--- HashCodePreparation/Result.cs
+++ HashCodePreparation/Result.cs
@@ -8,7 +8,6 @@ namespace HashCodePreparation
 {
     public class Result
     {
-        //index is cache center. Value is list of video indexes
-        public Dictionary<int, List<int>> Data { get; set; }
+ 
     }
 }
