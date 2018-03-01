82512fc4ecfcb5882e3c66e73c0bccc83119a499
MoaidHathot <moaid.hathot@outlook.com>
2018-03-01 21:46:00 +0200

    Parsing result

--- HashCodePreparation/Program.cs
+++ HashCodePreparation/Program.cs
@@ -10,10 +10,10 @@ namespace HashCodePreparation
     {
         static void Main(string[] args)
         {
-            //var input = ParseInput(args);
+            var input = ParseInput(args);
             //var input = CreateFakeInput();
 
-            var input = MockData();
+            //var input = MockData();
 
             var algorithm = new Algorithm();//SelectAlgorithm(input);
 
@@ -24,24 +24,24 @@ namespace HashCodePreparation
             //DeliverOutput(text);
         }
 
-        private static Input MockData()
-        {
-            return new Input()
-            {
-                Bonus = 2,
-                Columens = 4,
-                Rows = 3,
-                RideNumber = 3,
-                Steps = 10,
-                Vehicales = 2,
-                Rides = new List<Ride>()
-                {
-                    new Ride(0, 0, 1, 3, 2, 9, 0),
-                    new Ride(1,2,1,0, 0, 9, 1),
-                    new Ride(2,0,2,2, 0, 9, 2),
-                }
-            };
-        }
+        //private static Input MockData()
+        //{
+        //    return new Input
+        //    {
+        //        Bonus = 2,
+        //        Columens = 4,
+        //        Rows = 3,
+        //        RideNumber = 3,
+        //        Steps = 10,
+        //        Vehicales = 2,
+        //        Rides = new List<Ride>()
+        //        {
+        //            new Ride(0, 0, 1, 3, 2, 9, 0),
+        //            new Ride(1,2,1,0, 0, 9, 1),
+        //            new Ride(2,0,2,2, 0, 9, 2),
+        //        }
+        //    };
+        //}
 
         static Input ParseInput(string[] args)
         {
@@ -58,11 +58,7 @@ namespace HashCodePreparation
         {
             return new Output
             {
-                Result = new List<List<int>>
-                {
-                    new List<int>{ 1, 0},
-                    new List<int>{ 2, 2, 1}
-                }
+                Result = result.RidesForAllVehicles.OrderBy(pair => pair.Key).Select(pair => pair.Value.RidesTaken.Select(ride => ride.Index).ToList()).ToList()
             };
         }
 
